87edbb7f6e1cbf6b48f2d36fadbaa0287e861bfc
Gabriel <gabriel.rochat18@gmai.com>
2023-10-09 18:52:13 -0300

    login completo

--- app/App.js
+++ app/App.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from 'react-navigation/native';
-import AppStack from './AppStack'; // Importe o componente AppStack
-import LoginScreen from './src/screens/Login';
-import Header from './src/components/Header';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen'; // Corrija o caminho de importação
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
   return (
-    <NavigationContainer>
-      <AppStack />
-    </NavigationContainer>
+    <View style={styles.container}>
+      {loggedIn ? (
+        <Text>Você está logado! Redirecionar para a próxima tela aqui.</Text>
+      ) : (
+        <LoginScreen onLogin={handleLogin} />
+      )}
+    </View>
   );
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '40%',
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
   },
 });
+
+
+
+
 
 
